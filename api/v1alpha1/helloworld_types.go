4a8aa5610f1e13902777d6223b7b7d444b3cca73
zhuyunhao3 <zhuyunhao3@jd.com>
2023-02-08 11:31:01 +0800

    feat: first commit

---
+++ api/v1alpha1/helloworld_types.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HelloWorldSpec defines the desired state of HelloWorld
+type HelloWorldSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Size int32 `json:"size"`
+
+	MaxCpu int `json:"maxCpu"`
+	MinCpu int `json:"minCpu"`
+}
+
+// HelloWorldStatus defines the observed state of HelloWorld
+type HelloWorldStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Nodes []string `json:"nodes"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// HelloWorld is the Schema for the helloworlds API
+type HelloWorld struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HelloWorldSpec   `json:"spec,omitempty"`
+	Status HelloWorldStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// HelloWorldList contains a list of HelloWorld
+type HelloWorldList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HelloWorld `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&HelloWorld{}, &HelloWorldList{})
+}
